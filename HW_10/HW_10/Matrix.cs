8150bb2457a699994706097632391252ca75ad57
dextermd <dextermdpay@gmail.com>
2023-12-13 17:48:34 +0200

    HW_10

---
+++ HW_10/HW_10/Matrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ls_10
+{
+    internal class Matrix
+    {
+        //Пример двумерного индексатора
+        int[,] arr;
+        public int rows, cols; // Размерность двухмерного массива
+        public int Length;
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            arr = new int[this.rows, this.cols];
+            Length = rows * cols;
+        }
+        public int this[int index1, int index2] // Индексатор
+        {
+            get
+            {
+                return arr[index1, index2];
+            }
+            set
+            {
+                arr[index1, index2] = value;
+            }
+        }
+    }
+}
---
+++ HW_10/HW_10/MyArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ls_10
+{
+    internal class MyArray
+    {
+        private int[] arr;
+
+        public MyArray() : this(0) { }
+        public MyArray(int num)
+        {
+            arr = new int[num];
+        }
+        public MyArray(params object[] parameters)
+        {
+            int size = parameters.Length;
+            arr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = (int)parameters[i];
+            }
+        }
+        public void Show()
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > arr.Length)
+                {
+                    throw new ArgumentException("Error index");
+                }
+                return arr[index];
+            }
+            set
+            {
+                if (index < 0 || index > arr.Length)
+                {
+                    throw new ArgumentException("Error index");
+                }
+                arr[index] = value;
+            }
+        }  
+        public static MyArray operator +(MyArray objLeft, MyArray objRight)
+        {
+            return new MyArray() { arr = objLeft.arr.Concat(objRight.arr).ToArray() };
+        }
+    }
+}
