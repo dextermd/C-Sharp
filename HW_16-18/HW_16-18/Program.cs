e7eb87216c67804e08d56927c8863c6f1a103e1e
dextermd <dextermdpay@gmail.com>
2024-02-05 21:13:10 +0200

    Init

---
+++ HW_16-18/HW_16-18/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_16_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if true
+            //  Задание 1: Создайте generic класс Стека. Реализуйте стандартные методы и свойства для работы стека:
+            //    − pop;
+            //    − push;
+            //    − peek;
+            //    − count.
+            //  Подробнее об устройстве стека читайте тут: https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+
+            StackT<int> stack = new StackT<int>();
+
+            stack.Push(254);
+            stack.Push(12);
+            stack.Push(23);
+            stack.Push(101);
+
+
+            stack.Peek();
+            stack.Pop();
+
+            Console.WriteLine();
+
+            stack.Peek();
+
+
+#endif
+
+#if true
+            //  Задание 2: Создайте generic класс Очередь с приоритетами. Реализуйте стандартные методы и
+            //  свойства для работы очереди с приоритетами.
+            //  Подробности тут: https://en.wikipedia.org/wiki/Priority_queue
+#endif
+
+#if true
+            //  Задание 3: Создайте приложение для учёта сотрудников.Необходимо хранить следующую
+            //  информацию:
+            //    − ФИО
+            //    − Должность
+            //    − Заработная плата
+            //    − Рабочий email
+            //  Для хранения данных сотрудников используйте класс List<T>.Приложение должно предоставлять такую
+            //  функциональность:
+            //    − Добавление сотрудников
+            //    − Удаление сотрудников
+            //    − Изменение информации о сотрудниках
+            //    − Поиск сотрудников по параметрам
+            //    − Сортировка сотрудников по параметрам
+            //  Для удобства использования создайте пользовательское меню. Организуйте возможность тестирования
+            //  работы программы на уже готовом списке сотрудников
+#endif
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW_16-18/HW_16-18/StackT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_16_18
+{
+    internal class StackT<T>
+    {
+        private const int MAX_SIZE = 10;
+        private int size;
+        private T[] items;
+        private int top;
+
+        public StackT(int size = MAX_SIZE)
+        {
+            this.size = size;
+            items = new T[this.size];
+        }
+
+        public T Pop()
+        {
+            if (top > -1) 
+                return items[--top];
+            else Console.WriteLine("Stack is empty"); return default(T);
+        }
+        public void Push(T data) { if (top < size) items[top++] = data; }
+
+        public void Peek()
+        {
+            if (top == 0)
+                Console.WriteLine("Stack is empty");
+
+            for (int i = top - 1; i >  -1 ; i--)
+                Console.WriteLine($"{items[i]}");
+        }
+        public int GetCount() => top;
+
+    }
+}
